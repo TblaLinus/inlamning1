9b532695b118643bc033c66c339749925571abe8
Linus <osnusmeforfasensatan@hotmail.com>
2017-11-01 15:16:49 +0100

    trigger validation in modelWrapper

--- FriendOrganizer/FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer/FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,5 +1,6 @@
 ﻿using FriendOrganizer.Model;
 using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,43 +17,32 @@ namespace FriendOrganizer.UI.Wrapper
         public string FirstName
         {
             get { return GetValue<string>(); }
-            set
-            {
-                SetValue(value);
-                ValidateProperty(nameof(FirstName));
-            }
-        }
-
-        private void ValidateProperty(string propertyName)
-        {
-            ClearErrors(propertyName);
-            switch (propertyName)
-            {
-                case nameof(FirstName):
-                    if(string.Equals(FirstName, "Robot", StringComparison.OrdinalIgnoreCase))
-                    {
-                        AddError(propertyName, "Robots are not valid friends");
-                    }
-                    break;
-            }
+            set { SetValue(value); }
         }
 
         public string LastName
         {
             get { return GetValue<string>(); }
-            set
-            {
-                SetValue(value);
-            }
+            set { SetValue(value); }
         }
 
         public string Email
         {
             get { return GetValue<string>(); }
-            set
+            set { SetValue(value); }
+        }
+
+        protected override IEnumerable<string> ValidateProperty(string propertyName)
+        {
+            switch (propertyName)
             {
-                SetValue(value);
+                case nameof(FirstName):
+                    if (string.Equals(FirstName, "Robot", StringComparison.OrdinalIgnoreCase))
+                    {
+                        yield return "Robots are not valid friends";
+                    }
+                    break;
             }
         }
-    } 
+        } 
 }
--- FriendOrganizer/FriendOrganizer.UI/Wrapper/ModelWrapper.cs
+++ FriendOrganizer/FriendOrganizer.UI/Wrapper/ModelWrapper.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace FriendOrganizer.UI.Wrapper
 {
@@ -11,15 +14,35 @@ namespace FriendOrganizer.UI.Wrapper
 
         public T Model { get; }
 
+        protected virtual void SetValue<TValue>(TValue value, [CallerMemberName]string propertyName = null)
+        {
+            typeof(T).GetProperty(propertyName).SetValue(Model, value);
+            OnPropertyChanged(propertyName);
+            ValidatePropertyInternal(propertyName);
+        }
+
         protected virtual TValue GetValue<TValue>([CallerMemberName]string propertyName=null)
         {
             return (TValue) typeof(T).GetProperty(propertyName).GetValue(Model);
         }
 
-        protected virtual void SetValue<TValue>(TValue value, [CallerMemberName]string propertyName = null)
+        private void ValidatePropertyInternal(string propertyName)
         {
-            typeof(T).GetProperty(propertyName).SetValue(Model, value);
-            OnPropertyChanged(propertyName);
+            ClearErrors(propertyName);
+
+            var errors = ValidateProperty(propertyName);
+            if (errors != null)
+            {
+                foreach (var error in errors)
+                {
+                    AddError(propertyName, error);
+                }
+            }
+        }
+
+        protected virtual IEnumerable<string> ValidateProperty(string propertyName)
+        {
+            return null;
         }
     }
 }
