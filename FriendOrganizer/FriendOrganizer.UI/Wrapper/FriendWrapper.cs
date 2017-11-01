1ac09978d67744b18ac804d52e0990b5b0666393
Linus <osnusmeforfasensatan@hotmail.com>
2017-11-01 15:05:05 +0100

    refactoring

--- FriendOrganizer/FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer/FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,33 +1,24 @@
 ﻿using FriendOrganizer.Model;
-using FriendOrganizer.UI.ViewModel;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections;
 
 namespace FriendOrganizer.UI.Wrapper
 {
-    public class FriendWrapper : ViewModelBase, INotifyDataErrorInfo
+    public class FriendWrapper : ModelWrapper<Friend>
     {
-        public FriendWrapper(Friend model)
+        public FriendWrapper(Friend model) : base(model)
         {
-            Model = model;
         }
-
-        public Friend Model { get; }
 
         public int Id { get { return Model.Id; } }
 
         public string FirstName
         {
-            get { return Model.FirstName; }
+            get { return GetValue<string>(); }
             set
             {
-                Model.FirstName = value;
-                OnPropertyChanged();
+                SetValue(value);
                 ValidateProperty(nameof(FirstName));
             }
         }
@@ -48,59 +39,19 @@ namespace FriendOrganizer.UI.Wrapper
 
         public string LastName
         {
-            get { return Model.LastName; }
+            get { return GetValue<string>(); }
             set
             {
-                Model.LastName = value;
-                OnPropertyChanged();
+                SetValue(value);
             }
         }
 
         public string Email
         {
-            get { return Model.Email; }
+            get { return GetValue<string>(); }
             set
             {
-                Model.Email = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private Dictionary<string, List<string>> _errorsByPropertyName = new Dictionary<string, List<string>>();
-
-        public bool HasErrors => _errorsByPropertyName.Any();
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        public IEnumerable GetErrors(string propertyName)
-        {
-            return _errorsByPropertyName.ContainsKey(propertyName) ? _errorsByPropertyName[propertyName] : null;
-        }
-
-        private void OnErrorsChanged(string propertyName)
-        {
-            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-        }
-
-        private void AddError(string propertyName, string error)
-        {
-            if (!_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName[propertyName] = new List<string>();
-            }
-            if (!_errorsByPropertyName[propertyName].Contains(error))
-            {
-                _errorsByPropertyName[propertyName].Add(error);
-                OnErrorsChanged(propertyName);
-            }
-        }
-
-        private void ClearErrors(string propertyName)
-        {
-            if (!_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName.Remove(propertyName);
-                OnErrorsChanged(propertyName);
+                SetValue(value);
             }
         }
     } 
