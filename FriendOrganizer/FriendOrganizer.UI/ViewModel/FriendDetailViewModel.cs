6f45e8768bd76b42d89b6d3189366468730a7c77
Linus <osnusmeforfasensatan@hotmail.com>
2017-11-06 09:54:23 +0100

    added save PhoneNumber function

--- FriendOrganizer/FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -198,7 +198,11 @@ namespace FriendOrganizer.UI.ViewModel
 
         private void OnAddPhoneNumberExecute()
         {
-            //TODO: Implement
+            var newNumber = new FriendPhoneNumberWrapper(new FriendPhoneNumber());
+            newNumber.PropertyChanged += FriendPhoneNumberWrapper_PropertyChanged;
+            PhoneNumbers.Add(newNumber);
+            Friend.Model.PhoneNumbers.Add(newNumber.Model);
+            newNumber.Number = ""; //TriggerValidation
         }
 
         private void OnRemovePhoneNumberExecute()
