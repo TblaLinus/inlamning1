60e7cab073067e8fe1bf4b2e3257a1a9d7957aba
Linus <osnusmeforfasensatan@hotmail.com>
2017-11-06 16:17:44 +0100

    added meeting entity

--- FriendOrganizer/FriendOrganizer.DataAccess/Migrations/Configuration.cs
+++ FriendOrganizer/FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -2,6 +2,7 @@ namespace FriendOrganizer.DataAccess.Migrations
 {
     using FriendOrganizer.Model;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -31,6 +32,19 @@ namespace FriendOrganizer.DataAccess.Migrations
 
             context.FriendPhoneNumbers.AddOrUpdate(pn => pn.Number,
                 new FriendPhoneNumber { Number = "+49 12345678", FriendId = context.Friends.First().Id });
+
+            context.Meetings.AddOrUpdate(m => m.Title,
+                new Meeting
+                {
+                    Title = "Watching Football",
+                    DateFrom = new DateTime(2018, 5, 26),
+                    DateTo = new DateTime(2018, 5, 26),
+                    Friends = new List<Friend>
+                    {
+                        context.Friends.Single(f => f.FirstName == "Thomas" && f.LastName=="Huber"),
+                        context.Friends.Single(f => f.FirstName == "Andreas" && f.LastName=="Boehler"),
+                    }
+                });
         }
     }
 }
